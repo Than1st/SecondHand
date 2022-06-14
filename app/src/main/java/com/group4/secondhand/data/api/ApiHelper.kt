3aae80c7ef8898a2888bd43c26f2a44a04b6fff4
Farhan160200 <fadhlurrahmanfarhan16@gmail.com>
2022-06-14 19:34:28 +0700

    branch farhanfarkaan

--- app/src/main/java/com/group4/secondhand/data/api/ApiHelper.kt
+++ app/src/main/java/com/group4/secondhand/data/api/ApiHelper.kt
@@ -3,4 +3,5 @@ package com.group4.secondhand.data.api
 class ApiHelper(private val apiService: ApiService) {
 //    contoh api service
 //    suspend fun getSellerProduct(apiKey: String) = apiService.getSellerProduct(apiKey)
+
 }
