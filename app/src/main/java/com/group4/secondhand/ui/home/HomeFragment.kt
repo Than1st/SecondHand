16674724a50dee66825376b2b41578087ecb79ee
Musnadil <firdaus.musnadil@gmail.com>
2022-06-16 21:39:02 +0700

    search view

--- app/src/main/java/com/group4/secondhand/ui/home/HomeFragment.kt
+++ app/src/main/java/com/group4/secondhand/ui/home/HomeFragment.kt
@@ -1,15 +1,17 @@
 package com.group4.secondhand.ui.home
 
-import android.content.Context.MODE_PRIVATE
+import android.animation.ArgbEvaluator
+import android.animation.ValueAnimator
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.group4.secondhand.R
 import com.group4.secondhand.databinding.FragmentHomeBinding
-import com.group4.secondhand.ui.splashscreen.SplashscreenFragment.Companion.ONBOARDING_PREF
-import com.group4.secondhand.ui.splashscreen.SplashscreenFragment.Companion.SHARED_PREF
+
 
 class HomeFragment : Fragment() {
 
@@ -32,7 +34,60 @@ class HomeFragment : Fragment() {
 //        if (checkOnboarding){
 //            Toast.makeText(requireContext(), "Halo!", Toast.LENGTH_SHORT).show()
 //        }
-
+        binding.statusBar.layoutParams = ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,getStatusBarHeight())
+        changeToolbar()
     }
+
+    private fun getStatusBarHeight(): Int {
+        var result = 0
+        val resourceId = resources.getIdentifier("status_bar_height", "dimen", "android")
+        if (resourceId > 0) {
+            result = resources.getDimensionPixelSize(resourceId)
+        }
+        return result
+    }
+
+    private fun changeToolbar(){
+        var toolbarColored = false
+        var toolbarTransparent = true
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            binding.scrollView.setOnScrollChangeListener {_,_,scrollY,_,_->
+                val bannerHeight = (binding.imgBanner.height /2) - getStatusBarHeight() - binding.statusBar.height
+                val colored = ContextCompat.getColor(requireContext(), R.color.medium_blue)
+                val transparent = ContextCompat.getColor(requireContext(), android.R.color.transparent)
+
+                when {
+                    scrollY > bannerHeight ->{
+                        if (toolbarTransparent){
+                            val colorAnimate = ValueAnimator.ofObject(ArgbEvaluator(),transparent,colored)
+                            colorAnimate.addUpdateListener {
+                                binding.statusBar.setBackgroundColor(it.animatedValue as Int)
+                                binding.toolbar.setBackgroundColor(it.animatedValue as Int)
+                            }
+                            colorAnimate.duration = 250
+                            colorAnimate.start()
+                            toolbarColored = true
+                            toolbarTransparent = false
+                        }
+                    }
+                    else ->{
+                        if(toolbarColored){
+                            val colorAnimate = ValueAnimator.ofObject(ArgbEvaluator(),colored,transparent)
+                            colorAnimate.addUpdateListener {
+                                binding.statusBar.setBackgroundColor(it.animatedValue as Int)
+                                binding.toolbar.setBackgroundColor(it.animatedValue as Int)
+                            }
+                            colorAnimate.duration = 250
+                            colorAnimate.start()
+                            toolbarColored = false
+                            toolbarTransparent = true
+                        }
+                    }
+                }
+            }
+        }
+    }
+
 
 }
