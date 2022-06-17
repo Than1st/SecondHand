af32396ae416551d293c8d31f4f6fbf59e4f252a
Musnadil <firdaus.musnadil@gmail.com>
2022-06-17 19:26:34 +0700

    recyclerview list category

---
+++ app/src/main/java/com/group4/secondhand/ui/home/HomeViewModel.kt
@@ -0,0 +1,29 @@
+package com.group4.secondhand.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.group4.secondhand.data.Repository
+import com.group4.secondhand.data.api.Resource
+import com.group4.secondhand.data.api.model.ResponseCategoryHome
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(private val repository: Repository):ViewModel() {
+    private val _category = MutableLiveData<Resource<List<ResponseCategoryHome>>>()
+    val category : LiveData<Resource<List<ResponseCategoryHome>>> get() = _category
+
+    fun getCategoryHome(){
+        viewModelScope.launch {
+            _category.postValue(Resource.loading())
+            try {
+                _category.postValue(Resource.success(repository.getCategoryHome()))
+            }catch (e:Exception){
+                _category.postValue(Resource.error(e.localizedMessage?:"Error occured"))
+            }
+        }
+    }
+}
