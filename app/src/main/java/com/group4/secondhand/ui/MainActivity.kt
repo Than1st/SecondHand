6fe96672d8bd7264f583c9bc747083655609e71b
Than1st <1911500641@student.budiluhur.ac.id>
2022-06-21 21:09:41 +0700

    delete lengkapi detail xml and lengkapi detail kt

--- app/src/main/java/com/group4/secondhand/ui/MainActivity.kt
+++ app/src/main/java/com/group4/secondhand/ui/MainActivity.kt
@@ -24,6 +24,9 @@ class MainActivity : AppCompatActivity() {
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {
+                R.id.jualFragment -> {
+                    binding.bottomNavigation.visibility = View.GONE
+                }
                 R.id.previewProductFragment -> {
                     binding.bottomNavigation.visibility = View.GONE
                 }
